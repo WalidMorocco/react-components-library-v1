2a549a18513a468bb35da1e95c9e10b1a7c8c7eb
Walid Amar <walidamarpro@gmail.com>
2022-11-25 02:31:32 -0500

    work on pages

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,10 +2,10 @@ export  const MainPage = () => {
     
     return (
         <div className=''>
-            {/* <h1>ABCDEFGHUJKLMNIOQRSTUVWXYZ</h1>
+            <h1>ABCDEFGHUJKLMNIOQRSTUVWXYZ</h1>
             <h1>abcdefghijklmnopqrstuvwxyz</h1>
             <h2>abcdefghijklmnopqrstuvwxyz</h2>
-            <h3>abcdefghijklmnopqrstuvwxyz</h3> */}
+            <h3>abcdefghijklmnopqrstuvwxyz</h3>
         </div>
     )
 }
