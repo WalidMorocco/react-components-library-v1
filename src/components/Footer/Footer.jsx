30d1efbedf7d482e35188fd25942a49ade6e2aa6
WalidMorocco <walidamarpro@gmail.com>
2023-01-07 15:06:41 +0100

    need work on card text shrink

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import './styles.css'
 
+const d = new Date();
+let year = d.getFullYear();
+
 export const Footer = () => {
     
     return (
-        <div class="footer" >
-            <p>PsiDev | 2022</p>
+        <div class="footer">
+            <p>PsiDev | {year}</p>
         </div>
     )
 }
